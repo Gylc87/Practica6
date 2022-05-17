7cdcaa011a13d5996d683c1c2dc4adb68f6622a9
Gylc87 <a19110230@ceti.mx>
2022-05-16 20:17:40 -0500

    commit

---
+++ Practica6.py
@@ -0,0 +1,30 @@
+import cv2
+import numpy as np
+
+cap = cv2.VideoCapture(0)
+
+
+
+while True:
+    _, frame = cap.read()
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    
+
+    #HSV HUE, SAT, VALUE
+    rojo_bajo = np.array([30,150,50])
+    rojo_alto = np.array([255,255,180])
+
+    mascara = cv2.inRange(hsv, rojo_bajo, rojo_alto)
+    resultado = cv2.bitwise_and(frame, frame, mask= mascara)
+
+
+    cv2.imshow("Frame", frame)
+    cv2.imshow("Mascara", mascara)
+    cv2.imshow("Resultado", resultado)
+
+    k = cv2.waitKey(5) & 0xFF
+    if k ==27:
+        break
+
+cv2.destroyAllWindows()
+cap.release()
