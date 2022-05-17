9d043629fa5295542af9e4706e0e2307e3bf37a2
Gylc87 <a19110230@ceti.mx>
2022-05-16 22:20:07 -0500

    commit

--- Practica6.py
+++ Practica6.py
@@ -10,12 +10,16 @@ while True:
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
     
 
-    #HSV HUE, SAT, VALUE ROJO
-    rojo_bajo = np.array([150,150,20],np.uint8)
-    rojo_alto = np.array([179,255,255],np.uint8)
+    #HSV HUE, SAT, VALUE ROJO    
+    rojo_bajo1 = np.array([0,255,20],np.uint8)
+    rojo_alto1 = np.array([5,255,255],np.uint8)
+    rojo_bajo2 = np.array([175,100,20],np.uint8)
+    rojo_alto2 = np.array([179,255,255],np.uint8)
 
-    mascara_rojo = cv2.inRange(hsv, rojo_bajo, rojo_alto)
-    resultado_rojo = cv2.bitwise_and(frame, frame, mask= mascara_rojo)
+    mascara_rojo = cv2.inRange(hsv, rojo_bajo1, rojo_alto1)
+    mascara_rojo2 = cv2.inRange(hsv, rojo_bajo2, rojo_alto2)
+    resultado_rojo = cv2.add(mascara_rojo, mascara_rojo2)
+    resultado_rojo2 = cv2.bitwise_and(frame, frame, mask= resultado_rojo) 
 
     #HSV HUE, SAT, VALUE AZUL
     azul_bajo = np.array([100,100,20],np.uint8)
@@ -33,8 +37,9 @@ while True:
 
 
     cv2.imshow("Frame", frame)
+    
     cv2.imshow("Mascara Rojo", mascara_rojo)
-    cv2.imshow("Resultado Rojo", resultado_rojo)
+    cv2.imshow("Resultado Rojo", resultado_rojo2)
 
     cv2.imshow("Mascara Azul", mascara_azul)
     cv2.imshow("Resultado Azul", resultado_azul)
